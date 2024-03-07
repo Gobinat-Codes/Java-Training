46a147ed13c7afaf4df51d9cdef09793877fc69d
Gobinat-Codes <140052579+Gobinat-Codes@users.noreply.github.com>
2024-03-07 21:05:19 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,9 @@
+public class OddOrEven{
+	public static void main(String[] args){
+		for(int n = 11 ; n <= 20 ; n+=2){
+			System.out.println(n);
+		}
+	}
+}
+
+// odd
---
+++ OddOrEven2.java
@@ -0,0 +1,11 @@
+public class OddOrEven2{
+	public static void main(String[] args){
+		for(int o = 10 ; o<=20 ; o++){
+			if(o%2!=1){
+			System.out.println(o);
+		}
+	}
+	}
+}
+
+// even
