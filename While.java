2571a617085a0cb7c07c633cdd878d14a313e9bc
Gobinat-Codes <140052579+Gobinat-Codes@users.noreply.github.com>
2024-03-07 21:05:49 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,11 @@
+public class While{
+	public static void main(String[] args){
+		int i = 0;
+		while(i<=5){
+			System.out.println(i);
+			i++;
+		}
+	}
+}
+
+// loop runs while the condition is true
