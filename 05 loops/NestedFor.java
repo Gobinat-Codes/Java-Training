9de8f2457261936acf534cec94475727f7d77016
Gobinat-Codes <140052579+Gobinat-Codes@users.noreply.github.com>
2024-03-10 13:09:07 +0530

    Add files via upload

---
+++ 05 loops/NestedFor.java
@@ -0,0 +1,10 @@
+public class NestedFor{
+	public static void main(String[] args){
+		for(int i = 1 ; i <=3 ; i++){
+			for(int j = 1 ; j<=10 ; j++){
+			System.out.println("inn : " + j);
+			}
+			System.out.println("outer : " + i);
+		}
+	}
+}
